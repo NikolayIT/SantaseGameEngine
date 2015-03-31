2717822f6ed16434e3d5894cbe196f602e10a9ed
Nikolay Kostov <admin@nikolay.it>
2015-03-31 18:35:27 +0300

    Introduced GameHand logic class

---
+++ Source/Santase.Logic/GameHand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Santase.Logic
+{
+    public class GameHand : IGameHand
+    {
+        public GameHand()
+        {
+
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerPosition Winner
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Source/Santase.Logic/IGameHand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Santase.Logic
+{
+    public interface IGameHand
+    {
+        void Start();
+
+        PlayerPosition Winner { get; }
+    }
+}
