bf0229f3004854dad5b582f784164065070aa6f2
Nikolay Kostov <admin@nikolay.it>
2015-03-31 21:01:02 +0300

    Added skeleton for IPlayerActionValidater

---
+++ Source/Santase.Logic/IPlayerActionValidater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Santase.Logic.Players;
+namespace Santase.Logic
+{
+    public interface IPlayerActionValidater
+    {
+        bool IsValid(PlayerAction action, PlayerTurnContext context);
+    }
+}
---
+++ Source/Santase.Logic/PlayerActionValidater.cs
@@ -0,0 +1,18 @@
+﻿using Santase.Logic.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Santase.Logic
+{
+    public class PlayerActionValidater : IPlayerActionValidater
+    {
+        public bool IsValid(PlayerAction action, Players.PlayerTurnContext context)
+        {
+            // TODO: Implement
+            return false;
+        }
+    }
+}
