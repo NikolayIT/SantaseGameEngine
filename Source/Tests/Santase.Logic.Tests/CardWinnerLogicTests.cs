60fe96e52e32b7a399dd37457a92eaabab5cd1ae
Nikolay Kostov <admin@nikolay.it>
2015-08-24 19:51:00 +0300

    Added unit test for the CardWinnerLogic class

---
+++ Source/Tests/Santase.Logic.Tests/CardWinnerLogicTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Santase.Logic.Tests
+{
+    using NUnit.Framework;
+
+    using Santase.Logic.Cards;
+
+    [TestFixture]
+    public class CardWinnerLogicTests
+    {
+        private static readonly object[] FirstPlayerWins =
+            {
+                new object[] { new Card(CardSuit.Club, CardType.Ace), new Card(CardSuit.Club, CardType.Jack), CardSuit.Spade },
+                new object[] { new Card(CardSuit.Club, CardType.Ace), new Card(CardSuit.Diamond, CardType.Jack), CardSuit.Spade },
+                new object[] { new Card(CardSuit.Club, CardType.Jack), new Card(CardSuit.Diamond, CardType.Ace), CardSuit.Spade },
+                new object[] { new Card(CardSuit.Heart, CardType.Ten), new Card(CardSuit.Heart, CardType.Nine), CardSuit.Heart },
+                new object[] { new Card(CardSuit.Heart, CardType.Nine), new Card(CardSuit.Diamond, CardType.King), CardSuit.Heart },
+            };
+
+        private static readonly object[] SecondPlayerWins =
+            {
+                new object[] { new Card(CardSuit.Club, CardType.Ten), new Card(CardSuit.Club, CardType.Ace), CardSuit.Diamond },
+                new object[] { new Card(CardSuit.Club, CardType.Ten), new Card(CardSuit.Club, CardType.Ace), CardSuit.Club },
+                new object[] { new Card(CardSuit.Spade, CardType.Ace), new Card(CardSuit.Club, CardType.Nine), CardSuit.Club },
+            };
+
+        [TestCaseSource(nameof(FirstPlayerWins))]
+        public void FirstPlayerShouldWins(Card firstPlayerCard, Card secondPlayerCard, CardSuit trumpSuit)
+        {
+            var cardWinner = new CardWinnerLogic();
+            var result = cardWinner.Winner(firstPlayerCard, secondPlayerCard, trumpSuit);
+            Assert.AreEqual(PlayerPosition.FirstPlayer, result);
+        }
+
+        [TestCaseSource(nameof(SecondPlayerWins))]
+        public void SecondPlayerShouldWins(Card firstPlayerCard, Card secondPlayerCard, CardSuit trumpSuit)
+        {
+            var cardWinner = new CardWinnerLogic();
+            var result = cardWinner.Winner(firstPlayerCard, secondPlayerCard, trumpSuit);
+            Assert.AreEqual(PlayerPosition.SecondPlayer, result);
+        }
+    }
+}
