9e35056dcd6409d7d435ec414dfbcf6a5a959740
Nikolay Kostov <admin@nikolay.it>
2015-08-25 14:55:01 +0300

    Refactored PlayerActionValidater class

--- Source/Santase.Logic/PlayerActionValidater.cs
+++ Source/Santase.Logic/PlayerActionValidater.cs
@@ -18,58 +18,15 @@
 
             if (action.Type == PlayerActionType.PlayCard)
             {
-                if (!playerCards.Contains(action.Card))
+                if (!CanPlayCard(action, context, playerCards))
                 {
                     return false;
                 }
-
-                if (action.Announce != Announce.None)
-                {
-                    if (action.Card.Type != CardType.Queen
-                        && action.Card.Type != CardType.King)
-                    {
-                        action.Announce = Announce.None;
-                    }
-
-                    // TODO: Check for another card
-                }
-
-                if (context.State.ShouldObserveRules)
-                {
-                    if (!context.AmITheFirstPlayer)
-                    {
-                        var firstCard = context.FirstPlayedCard;
-                        var ourCard = action.Card;
-
-                        if (firstCard.Suit != ourCard.Suit)
-                        {
-                            if (ourCard.Suit != context.TrumpCard.Suit)
-                            {
-                                var hasTrump = playerCards.Any(c => c.Suit == context.TrumpCard.Suit);
-                                if (hasTrump)
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (ourCard.GetValue() < firstCard.GetValue())
-                            {
-                                var hasBigger = playerCards.Any(c => c.GetValue() > firstCard.GetValue());
-                                if (hasBigger)
-                                {
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                }
             }
 
-            if (action.Type == PlayerActionType.CloseGame && !(context.State is FinalRoundState))
+            if (action.Type == PlayerActionType.CloseGame)
             {
-                if (!context.State.CanClose || !context.AmITheFirstPlayer)
+                if (!CanCloseGame(context))
                 {
                     return false;
                 }
@@ -77,15 +34,87 @@
 
             if (action.Type == PlayerActionType.ChangeTrump)
             {
-                if (!context.State.CanChangeTrump || !context.AmITheFirstPlayer)
+                if (!CanChangeTrump(context, playerCards))
                 {
                     return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool CanCloseGame(PlayerTurnContext context)
+        {
+            if (!context.State.CanClose || !context.AmITheFirstPlayer)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool CanPlayCard(PlayerAction action, PlayerTurnContext context, IList<Card> playerCards)
+        {
+            if (!playerCards.Contains(action.Card))
+            {
+                return false;
+            }
+
+            if (action.Announce != Announce.None)
+            {
+                if (action.Card.Type != CardType.Queen && action.Card.Type != CardType.King)
+                {
+                    action.Announce = Announce.None;
                 }
 
-                if (!playerCards.Contains(new Card(context.TrumpCard.Suit, CardType.Nine)))
+                // TODO: Check for another card
+            }
+
+            if (context.State.ShouldObserveRules)
+            {
+                if (!context.AmITheFirstPlayer)
                 {
-                    return false;
+                    var firstCard = context.FirstPlayedCard;
+                    var ourCard = action.Card;
+
+                    if (firstCard.Suit != ourCard.Suit)
+                    {
+                        if (ourCard.Suit != context.TrumpCard.Suit)
+                        {
+                            var hasTrump = playerCards.Any(c => c.Suit == context.TrumpCard.Suit);
+                            if (hasTrump)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (ourCard.GetValue() < firstCard.GetValue())
+                        {
+                            var hasBigger = playerCards.Any(c => c.GetValue() > firstCard.GetValue());
+                            if (hasBigger)
+                            {
+                                return false;
+                            }
+                        }
+                    }
                 }
+            }
+
+            return true;
+        }
+
+        private static bool CanChangeTrump(PlayerTurnContext context, IList<Card> playerCards)
+        {
+            if (!context.State.CanChangeTrump || !context.AmITheFirstPlayer)
+            {
+                return false;
+            }
+
+            if (!playerCards.Contains(new Card(context.TrumpCard.Suit, CardType.Nine)))
+            {
+                return false;
             }
 
             return true;
