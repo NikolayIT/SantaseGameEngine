83dda0e575b4f875e2078ce772e8fb522278f987
Nikolay Kostov <admin@nikolay.it>
2015-08-25 19:47:31 +0300

    Added unit tests for the ChangeTrumpActionValidator class

---
+++ Source/Tests/Santase.Logic.Tests/PlayerActionValidate/ChangeTrumpActionValidatorTests.cs
@@ -0,0 +1,85 @@
+﻿namespace Santase.Logic.Tests.PlayerActionValidate
+{
+    using System.Collections.Generic;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using Santase.Logic.Cards;
+    using Santase.Logic.PlayerActionValidate;
+    using Santase.Logic.RoundStates;
+
+    [TestFixture]
+    public class ChangeTrumpActionValidatorTests
+    {
+        private static readonly Card TrumpThatCanBeChanged = new Card(CardSuit.Spade, CardType.Queen);
+
+        private static readonly Card TrumpThatCannotBeChanged = new Card(CardSuit.Diamond, CardType.King);
+
+        private static readonly IList<Card> PlayerCards = new List<Card>
+                                                              {
+                                                                  new Card(CardSuit.Club, CardType.Nine),
+                                                                  new Card(CardSuit.Spade, CardType.Nine),
+                                                                  new Card(CardSuit.Club, CardType.Ten),
+                                                                  new Card(CardSuit.Spade, CardType.Ten),
+                                                                  new Card(CardSuit.Diamond, CardType.Ace),
+                                                                  new Card(CardSuit.Heart, CardType.Ace)
+                                                              };
+
+        [Test]
+        public void CanChangeTrumpShouldReturnFalseWhenThePlayerIsNotFirstButTheStatePermitsChanging()
+        {
+            var haveStateMock = new Mock<IHaveState>();
+            var roundState = new MoreThanTwoCardsLeftRoundState(haveStateMock.Object);
+
+            var validator = new ChangeTrumpActionValidator();
+            var canChangeTrump = validator.CanChangeTrump(false, roundState, TrumpThatCanBeChanged, PlayerCards);
+            Assert.IsFalse(canChangeTrump);
+        }
+
+        [Test]
+        public void CanChangeTrumpShouldReturnFalseWhenThePlayerIsFirstButTheStateDoesNotPermitChanging()
+        {
+            var haveStateMock = new Mock<IHaveState>();
+            var roundState = new FinalRoundState(haveStateMock.Object);
+
+            var validator = new ChangeTrumpActionValidator();
+            var canChangeTrump = validator.CanChangeTrump(true, roundState, TrumpThatCanBeChanged, PlayerCards);
+            Assert.IsFalse(canChangeTrump);
+        }
+
+        [Test]
+        public void CanChangeTrumpShouldReturnFalseWhenThePlayerIsNotFirstAndTheStateDoesNotPermitChanging()
+        {
+            var haveStateMock = new Mock<IHaveState>();
+            var roundState = new StartRoundState(haveStateMock.Object);
+
+            var validator = new ChangeTrumpActionValidator();
+            var canChangeTrump = validator.CanChangeTrump(false, roundState, TrumpThatCanBeChanged, PlayerCards);
+            Assert.IsFalse(canChangeTrump);
+        }
+
+        [Test]
+        public void CanChangeTrumpShouldReturnFalseWhenThePlayerIsFirstAndTheStatePermitsChangingButNineOfTrumpsIsNotPresent()
+        {
+            var haveStateMock = new Mock<IHaveState>();
+            var roundState = new MoreThanTwoCardsLeftRoundState(haveStateMock.Object);
+
+            var validator = new ChangeTrumpActionValidator();
+            var canChangeTrump = validator.CanChangeTrump(true, roundState, TrumpThatCannotBeChanged, PlayerCards);
+            Assert.IsFalse(canChangeTrump);
+        }
+
+        [Test]
+        public void CanChangeTrumpShouldReturnTrueWhenThePlayerIsFirstTheStatePermitsChangingAndNineOfTrumpsIsPresent()
+        {
+            var haveStateMock = new Mock<IHaveState>();
+            var roundState = new MoreThanTwoCardsLeftRoundState(haveStateMock.Object);
+
+            var validator = new ChangeTrumpActionValidator();
+            var canChangeTrump = validator.CanChangeTrump(true, roundState, TrumpThatCanBeChanged, PlayerCards);
+            Assert.IsTrue(canChangeTrump);
+        }
+    }
+}
