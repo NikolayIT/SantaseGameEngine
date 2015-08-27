526d46dbdcb86c2d048eba076283fb1dfc92e352
Nikolay Kostov <admin@nikolay.it>
2015-08-27 14:17:28 +0300

    Added unit tests for the MemoryLogger

---
+++ Source/Tests/Santase.Logic.Tests/Logger/MemoryLoggerTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Santase.Logic.Tests.Logger
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Santase.Logic.Logger;
+
+    [TestFixture]
+    public class MemoryLoggerTests
+    {
+        [Test]
+        public void LogLineShouldAppendLineAtTheEnd()
+        {
+            const string Message = "test";
+            var logger = new MemoryLogger();
+            logger.LogLine(Message);
+            Assert.AreEqual(Message + Environment.NewLine, logger.ToString());
+        }
+
+        [Test]
+        public void LogShouldAppendTheTextWhenCalledTwoTimesInARow()
+        {
+            var logger = new MemoryLogger();
+            logger.Log("test");
+            logger.Log("абвг");
+            Assert.AreEqual("testабвг", logger.ToString());
+        }
+
+        [Test]
+        public void LogLineShouldAppendLineBetweenTwoLogCalls()
+        {
+            const string FirstMessage = "test";
+            const string SecondMessage = "абвг";
+            var logger = new MemoryLogger();
+            logger.LogLine(FirstMessage);
+            logger.Log(SecondMessage);
+            Assert.AreEqual(FirstMessage + Environment.NewLine + SecondMessage, logger.ToString());
+        }
+    }
+}
