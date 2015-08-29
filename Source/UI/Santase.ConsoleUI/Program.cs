e00de033f22f03d0aab89f0c44b7724fbff1c544
Nikolay Kostov <admin@nikolay.it>
2015-08-29 12:33:59 +0300

    Refactorings in console program

--- Source/UI/Santase.ConsoleUI/Program.cs
+++ Source/UI/Santase.ConsoleUI/Program.cs
@@ -12,19 +12,42 @@
     {
         public static void Main()
         {
-            //// Console.BufferHeight = Console.WindowHeight = 17;
-            //// Console.BufferWidth = Console.WindowWidth = 50;
-
-            IPlayer firstPlayer = new DummyPlayer("First Dummy Player", new NoLogger()); // new ConsoleLogger("[1] ")); // new ConsolePlayer(5, 10);
-            IPlayer secondPlayer = new DummyPlayer("Second Dummy Player", new NoLogger()); // new ConsoleLogger("[2] ")); // new ConsolePlayer(10, 10);
-
-            ISantaseGame game = new SantaseGame(firstPlayer, secondPlayer, PlayerPosition.FirstPlayer, new ConsoleLogger("[game] "));
-
+            var game = CreateGameVersusBot();
             game.Start();
 
             Console.WriteLine("Game finished!");
             Console.WriteLine("{0} - {1}", game.FirstPlayerTotalPoints, game.SecondPlayerTotalPoints);
             Console.WriteLine("Rounds: {0}", game.RoundsPlayed);
+        }
+
+        private static ISantaseGame CreateTwoPlayerGame()
+        {
+            Console.BufferHeight = Console.WindowHeight = 17;
+            Console.BufferWidth = Console.WindowWidth = 50;
+
+            IPlayer firstPlayer = new ConsolePlayer(5, 10);
+            IPlayer secondPlayer = new ConsolePlayer(10, 10);
+            ISantaseGame game = new SantaseGame(firstPlayer, secondPlayer);
+            return game;
+        }
+
+        private static ISantaseGame CreateGameVersusBot()
+        {
+            Console.BufferHeight = Console.WindowHeight = 17;
+            Console.BufferWidth = Console.WindowWidth = 50;
+
+            IPlayer firstPlayer = new ConsolePlayer(5, 10);
+            IPlayer secondPlayer = new DummyPlayer("Second Dummy Player", new NoLogger()); // new ConsoleLogger("[2] "));
+            ISantaseGame game = new SantaseGame(firstPlayer, secondPlayer);
+            return game;
+        }
+
+        private static ISantaseGame CreateGameWithBots()
+        {
+            IPlayer firstPlayer = new DummyPlayer("First Dummy Player", new NoLogger()); // new ConsoleLogger("[1] "));
+            IPlayer secondPlayer = new DummyPlayer("Second Dummy Player", new NoLogger()); // new ConsoleLogger("[2] "));
+            ISantaseGame game = new SantaseGame(firstPlayer, secondPlayer, PlayerPosition.FirstPlayer, new ConsoleLogger("[game] "));
+            return game;
         }
     }
 }
