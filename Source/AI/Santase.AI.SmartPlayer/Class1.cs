012bc16a8b00b6336da52a815494a1f7a104e66b
Nikolay Kostov <admin@nikolay.it>
2015-08-31 19:28:38 +0300

    Created new project Santase.AI.SmartPlayer

---
+++ Source/AI/Santase.AI.SmartPlayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Santase.AI.SmartPlayer
+{
+    public class Class1
+    {
+    }
+}
