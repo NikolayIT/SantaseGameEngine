e395bce2dbfd5404ef3b0fcf62594a65af22f89a
Nikolay Kostov <admin@nikolay.it>
2015-08-31 19:30:35 +0300

    Implemented BasePlayer in SmartPlayer.cs

---
+++ Source/AI/Santase.AI.SmartPlayer/SmartPlayer.cs
@@ -0,0 +1,19 @@
+﻿namespace Santase.AI.SmartPlayer
+{
+    using Santase.Logic.Players;
+
+    public class SmartPlayer : BasePlayer
+    {
+        public override string Name => "Smart Player 150831";
+
+        public override PlayerAction GetTurn(PlayerTurnContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void EndTurn(PlayerTurnContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
