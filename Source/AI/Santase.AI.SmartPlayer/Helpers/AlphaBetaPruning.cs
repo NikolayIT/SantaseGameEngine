850349099960ac5ab6856921dd85541b4b8c4566
Nikolay Kostov <admin@nikolay.it>
2015-09-01 17:15:04 +0300

    Started working on AlphaBetaPruning algorithm for determining the winner when the player cards are known

---
+++ Source/AI/Santase.AI.SmartPlayer/Helpers/AlphaBetaPruning.cs
@@ -0,0 +1,51 @@
+﻿namespace Santase.AI.SmartPlayer.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Santase.Logic;
+    using Santase.Logic.Cards;
+    using Santase.Logic.PlayerActionValidate;
+    using Santase.Logic.Players;
+
+    public class AlphaBetaPruning
+    {
+        private readonly IPlayerActionValidator playerActionValidator;
+
+        private readonly ICardWinnerLogic cardWinnerLogic;
+
+        public AlphaBetaPruning(IPlayerActionValidator playerActionValidator, ICardWinnerLogic cardWinnerLogic)
+        {
+            this.playerActionValidator = playerActionValidator;
+            this.cardWinnerLogic = cardWinnerLogic;
+        }
+
+        public void GetBestCard(
+            IList<Card> firstPlayerCards,
+            IList<Card> secondPlayerCards,
+            PlayerTurnContext context,
+            int firstPlayerPoints,
+            int secondPlayerPoints)
+        {
+            var playerCards = context.IsFirstPlayerTurn ? firstPlayerCards : secondPlayerCards;
+            foreach (var playerCard in playerCards)
+            {
+                if (this.playerActionValidator.IsValid(PlayerAction.PlayCard(playerCard), context, playerCards))
+                {
+                    playerCards.Remove(playerCard);
+                    
+                    this.GetBestCard(
+                        firstPlayerCards,
+                        secondPlayerCards,
+                        context,
+                        firstPlayerPoints,
+                        secondPlayerPoints);
+
+                    playerCards.Add(playerCard);
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
