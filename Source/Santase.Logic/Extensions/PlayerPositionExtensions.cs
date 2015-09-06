3c6be811dc97471e307be2f359c7af73ff8f8fe5
Nikolay Kostov <admin@nikolay.it>
2015-09-06 13:57:01 +0300

    Added extension method OtherPlayer() on PlayerPositions values

---
+++ Source/Santase.Logic/Extensions/PlayerPositionExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Santase.Logic.Extensions
+{
+    using System;
+
+    public static class PlayerPositionExtensions
+    {
+        public static PlayerPosition OtherPlayer(this PlayerPosition playerPosition)
+        {
+            switch (playerPosition)
+            {
+                case PlayerPosition.FirstPlayer:
+                    return PlayerPosition.SecondPlayer;
+                case PlayerPosition.SecondPlayer:
+                    return PlayerPosition.FirstPlayer;
+                case PlayerPosition.NoOne:
+                    return PlayerPosition.NoOne;
+                default:
+                    throw new ArgumentException("Invalid PlayerPosition value", nameof(playerPosition));
+            }
+        }
+    }
+}
