cd848554fcd05be6e48af5df9b181bdbf3ff570c
Nikolay Kostov <admin@nikolay.it>
2015-09-06 15:52:36 +0300

    Implemented FileLogger

---
+++ Source/Santase.Logic/Logger/FileLogger.cs
@@ -0,0 +1,24 @@
+﻿namespace Santase.Logic.Logger
+{
+    using System.IO;
+
+    public class FileLogger : ILogger
+    {
+        private readonly TextWriter writer;
+
+        public FileLogger(string filePath)
+        {
+            this.writer = new StreamWriter(filePath);
+        }
+
+        public void Log(string message)
+        {
+            this.writer.Write(message);
+        }
+
+        public void LogLine(string message)
+        {
+            this.writer.WriteLine(message);
+        }
+    }
+}
