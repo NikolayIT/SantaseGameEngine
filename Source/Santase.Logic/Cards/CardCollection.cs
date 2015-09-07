1eae113a0a903292c39af76c3bdc9347dc77f0bf
Nikolay Kostov <admin@nikolay.it>
2015-09-07 17:12:38 +0300

    Implemented low memory CardCollection

---
+++ Source/Santase.Logic/Cards/CardCollection.cs
@@ -0,0 +1,143 @@
+﻿namespace Santase.Logic.Cards
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Low memory (only 8 bytes per instance) implementation of card collection.
+    /// </summary>
+    public class CardCollection : ICollection<Card>
+    {
+        private const int MaxCards = 52;
+
+        private long cards; // 64 bits for 52 possible cards
+
+        public int Count
+        {
+            get
+            {
+                var bits = this.cards;
+                var count = 0;
+                while (bits > 0)
+                {
+                    var bit = bits & 1;
+                    if (bit == 1)
+                    {
+                        count++;
+                    }
+
+                    bits = bits >> 1;
+                }
+
+                return count;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public IEnumerator<Card> GetEnumerator()
+        {
+            return new CardCollectionEnumerator(this.cards);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void Add(Card item)
+        {
+            this.cards |= (long)((long)1 << item.GetHashCode());
+        }
+
+        public void Clear()
+        {
+            this.cards = 0;
+        }
+
+        public bool Contains(Card item)
+        {
+            return ((this.cards >> item.GetHashCode()) & 1) == 1;
+        }
+
+        public void CopyTo(Card[] array, int arrayIndex)
+        {
+            foreach (var card in this)
+            {
+                array.SetValue(card, arrayIndex);
+                arrayIndex = arrayIndex + 1;
+            }
+        }
+
+        public bool Remove(Card item)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+
+            this.cards &= ~((long)1 << item.GetHashCode());
+
+            return true;
+        }
+
+        private class CardCollectionEnumerator : IEnumerator<Card>
+        {
+            private static readonly Card[] AllCards;
+
+            private readonly long cards;
+
+            private int currentHashCode;
+
+            static CardCollectionEnumerator()
+            {
+                AllCards = new Card[MaxCards + 1];
+
+                foreach (CardSuit cardSuitValue in Enum.GetValues(typeof(CardSuit)))
+                {
+                    foreach (CardType cardTypeValue in Enum.GetValues(typeof(CardType)))
+                    {
+                        var card = new Card(cardSuitValue, cardTypeValue);
+                        var hashCode = card.GetHashCode();
+                        AllCards[hashCode] = card;
+                    }
+                }
+            }
+
+            public CardCollectionEnumerator(long cards)
+            {
+                this.cards = cards;
+                this.Reset();
+            }
+
+            public Card Current => AllCards[this.currentHashCode];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                while (this.currentHashCode <= MaxCards)
+                {
+                    this.currentHashCode++;
+                    var bit = (this.cards >> this.currentHashCode) & 1;
+                    if (bit == 1)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                this.currentHashCode = -1;
+            }
+        }
+    }
+}
---
+++ Source/Tests/Santase.Logic.Tests/Cards/CardCollectionTests.cs
@@ -0,0 +1,191 @@
+﻿namespace Santase.Logic.Tests.Cards
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using Santase.Logic.Cards;
+
+    [TestFixture]
+    public class CardCollectionTests
+    {
+        [Test]
+        public void IsReadOnlyShouldReturnFalse()
+        {
+            Assert.IsFalse(new CardCollection().IsReadOnly);
+        }
+
+        [Test]
+        public void CountShouldReturn0WhenCardCollectionIsInitialized()
+        {
+            Assert.AreEqual(0, new CardCollection().Count);
+        }
+
+        [Test]
+        public void CountShouldReturn1WhenOneCardIsAdded()
+        {
+            var collection = new CardCollection();
+            collection.Add(new Card(CardSuit.Club, CardType.Ace));
+            Assert.AreEqual(1, collection.Count);
+        }
+
+        [Test]
+        public void CountShouldReturn1WhenOneCardIsAddedAndThenRemoved()
+        {
+            var collection = new CardCollection();
+            var card = new Card(CardSuit.Club, CardType.Ace);
+            collection.Add(card);
+            collection.Remove(card);
+            Assert.AreEqual(0, collection.Count);
+        }
+
+        [Test]
+        public void CountShouldReturnCorrectValueAfterFewCardAdds()
+        {
+            var collection = new CardCollection();
+            collection.Add(new Card(CardSuit.Club, CardType.Ace));
+            collection.Add(new Card(CardSuit.Heart, CardType.Ten));
+            collection.Add(new Card(CardSuit.Heart, CardType.King));
+            collection.Add(new Card(CardSuit.Diamond, CardType.Queen));
+            collection.Add(new Card(CardSuit.Spade, CardType.Jack));
+            collection.Add(new Card(CardSuit.Spade, CardType.Nine));
+            Assert.AreEqual(6, collection.Count);
+        }
+
+        [Test]
+        public void CountShouldReturnCorrectValueAfterAddingAllCards()
+        {
+            var collection = new CardCollection();
+            foreach (CardSuit cardSuitValue in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardType cardTypeValue in Enum.GetValues(typeof(CardType)))
+                {
+                    var card = new Card(cardSuitValue, cardTypeValue);
+                    collection.Add(card);
+                }
+            }
+
+            Assert.AreEqual(24, collection.Count);
+        }
+
+        [Test]
+        public void ContainsShouldReturnTrueForAllCardsAfterAddingThem()
+        {
+            var collection = new CardCollection();
+            foreach (CardSuit cardSuitValue in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardType cardTypeValue in Enum.GetValues(typeof(CardType)))
+                {
+                    var card = new Card(cardSuitValue, cardTypeValue);
+                    collection.Add(card);
+                }
+            }
+
+            foreach (CardSuit cardSuitValue in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardType cardTypeValue in Enum.GetValues(typeof(CardType)))
+                {
+                    var card = new Card(cardSuitValue, cardTypeValue);
+                    Assert.IsTrue(collection.Contains(card));
+                }
+            }
+        }
+
+        [Test]
+        public void ClearShouldReturn0Cards()
+        {
+            var collection = new CardCollection();
+            collection.Add(new Card(CardSuit.Club, CardType.Ace));
+            collection.Add(new Card(CardSuit.Diamond, CardType.Ten));
+            collection.Add(new Card(CardSuit.Heart, CardType.Jack));
+            collection.Add(new Card(CardSuit.Spade, CardType.Nine));
+            collection.Clear();
+            Assert.AreEqual(0, collection.Count);
+            Assert.AreEqual(0, collection.ToList().Count);
+        }
+
+        [Test]
+        public void RemoveNonExistingCardsShouldNotRemoveThem()
+        {
+            var collection = new CardCollection();
+            collection.Add(new Card(CardSuit.Spade, CardType.Ace));
+            collection.Remove(new Card(CardSuit.Club, CardType.Ace));
+            Assert.AreEqual(1, collection.Count);
+        }
+
+        [Test]
+        public void RemoveNullCardShouldNotDoAnything()
+        {
+            var collection = new CardCollection();
+            collection.Add(new Card(CardSuit.Spade, CardType.Ace));
+            collection.Remove(null);
+            Assert.AreEqual(1, collection.Count);
+        }
+
+        [Test]
+        public void RemoveShouldWorkProperly()
+        {
+            var card1 = new Card(CardSuit.Club, CardType.Ace); // 1
+            var card2 = new Card(CardSuit.Spade, CardType.King); // 52
+            var collection = new CardCollection { card1, card2 };
+            collection.Remove(card1);
+            collection.Remove(card2);
+            Assert.AreEqual(0, collection.Count);
+        }
+
+        [Test]
+        public void EnumerableGetEnumeratorShouldReturnNonNullEnumeratorWhichWorksCorrectly()
+        {
+            var card = new Card(CardSuit.Spade, CardType.King); // 52
+            IEnumerable collection = new CardCollection() { card };
+            var enumerator = collection.GetEnumerator();
+            Assert.IsNotNull(enumerator);
+            enumerator.MoveNext();
+            Assert.AreEqual(card, enumerator.Current);
+        }
+
+        [Test]
+        public void GetEnumeratorShouldReturnAllElementsInCollection()
+        {
+            var cards = new List<Card>
+                            {
+                                new Card(CardSuit.Club, CardType.Ace),
+                                new Card(CardSuit.Spade, CardType.Ace),
+                                new Card(CardSuit.Diamond, CardType.Ten),
+                                new Card(CardSuit.Heart, CardType.Jack),
+                                new Card(CardSuit.Club, CardType.Nine),
+                                new Card(CardSuit.Spade, CardType.Nine)
+                            };
+
+            var collection = new CardCollection();
+            foreach (var card in cards)
+            {
+                collection.Add(card);
+            }
+
+            var count = 0;
+            foreach (var card in collection)
+            {
+                Assert.IsTrue(cards.Contains(card), $"Card {card} not found in collection!");
+                count++;
+            }
+
+            Assert.AreEqual(cards.Count, count);
+        }
+
+        [Test]
+        public void CopyToShouldWorkProperly()
+        {
+            var card1 = new Card(CardSuit.Club, CardType.Ace); // 1
+            var card2 = new Card(CardSuit.Spade, CardType.King); // 52
+            var collection = new CardCollection { card1, card2 };
+            var array = new Card[2];
+            collection.CopyTo(array, 0);
+            Assert.IsTrue(array.Contains(card1));
+            Assert.IsTrue(array.Contains(card2));
+        }
+    }
+}
