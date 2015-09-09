ccdc7546802aa28c65d4e70ee4323503770dda96
Nikolay Kostov <admin@nikolay.it>
2015-09-09 13:52:55 +0300

    Added test for the Trick class: - PlayShouldCallGetTurnOnlyForFirstPlayerWhenTheFirstPlayerGoesOutByAnnounce

--- Source/Tests/Santase.Logic.Tests/GameMechanics/TrickTests.cs
+++ Source/Tests/Santase.Logic.Tests/GameMechanics/TrickTests.cs
@@ -1,9 +1,6 @@
 ﻿namespace Santase.Logic.Tests.GameMechanics
 {
     using System.Linq;
-    using System.Security.Cryptography.X509Certificates;
-
-    using Moq;
 
     using NUnit.Framework;
 
@@ -27,11 +24,45 @@
 
             SimulateGame(firstPlayerInfo, secondPlayerInfo, deck);
 
-            var trick = new Trick(firstPlayerInfo, secondPlayerInfo, stateManager, deck);
-            trick.Play();
+            var trick = new Trick(firstPlayerInfo, secondPlayerInfo, stateManager, deck, GameRulesProvider.Santase);
+            var winner = trick.Play();
 
             Assert.AreEqual(1, firstPlayer.GetTurnCalledCount);
             Assert.AreEqual(1, secondPlayer.GetTurnCalledCount);
+            Assert.IsTrue(winner == firstPlayerInfo || winner == secondPlayerInfo);
+        }
+
+        [Test]
+        public void PlayShouldCallGetTurnOnlyForFirstPlayerWhenTheFirstPlayerGoesOutByAnnounce()
+        {
+            var firstPlayer = new ValidPlayer();
+            var firstPlayerInfo = new RoundPlayerInfo(firstPlayer);
+            var secondPlayer = new ValidPlayer();
+            var secondPlayerInfo = new RoundPlayerInfo(secondPlayer);
+            var stateManager = new StateManager();
+            var deck = new Deck();
+
+            // 53 points in firstPlayerInfo.TrickCards
+            firstPlayerInfo.TrickCards.Add(new Card(CardSuit.Diamond, CardType.Ace));
+            firstPlayerInfo.TrickCards.Add(new Card(CardSuit.Diamond, CardType.Ten));
+            firstPlayerInfo.TrickCards.Add(new Card(CardSuit.Spade, CardType.Ace));
+            firstPlayerInfo.TrickCards.Add(new Card(CardSuit.Club, CardType.Ace));
+            firstPlayerInfo.TrickCards.Add(new Card(CardSuit.Club, CardType.Ten));
+
+            // Add cards for announcing 20
+            firstPlayerInfo.AddCard(new Card(CardSuit.Heart, CardType.King));
+            firstPlayerInfo.AddCard(new Card(CardSuit.Heart, CardType.Queen));
+            stateManager.SetState(new MoreThanTwoCardsLeftRoundState(stateManager));
+
+            secondPlayerInfo.AddCard(new Card(CardSuit.Heart, CardType.Ten));
+            secondPlayerInfo.AddCard(new Card(CardSuit.Heart, CardType.Ace));
+
+            var trick = new Trick(firstPlayerInfo, secondPlayerInfo, stateManager, deck, GameRulesProvider.Santase);
+            var winner = trick.Play();
+
+            Assert.AreEqual(1, firstPlayer.GetTurnCalledCount);
+            Assert.AreEqual(0, secondPlayer.GetTurnCalledCount);
+            Assert.AreSame(firstPlayerInfo, winner);
         }
 
         private static void SimulateGame(RoundPlayerInfo firstPlayer, RoundPlayerInfo secondPlayer, Deck deck)
@@ -53,9 +84,12 @@
 
             public int GetTurnCalledCount { get; private set; }
 
+            public PlayerTurnContext GetTurnContextObject { get; private set; }
+
             public override PlayerAction GetTurn(PlayerTurnContext context)
             {
                 this.GetTurnCalledCount++;
+                this.GetTurnContextObject = context.Clone() as PlayerTurnContext;
                 var possibleCardsToPlay = this.PlayerActionValidator.GetPossibleCardsToPlay(context, this.Cards);
                 return this.PlayCard(possibleCardsToPlay.First());
             }
