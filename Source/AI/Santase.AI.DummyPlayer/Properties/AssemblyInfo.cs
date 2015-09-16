4e721f77a6cd310c5ae43d77f4ed64acf714fdb0
Nikolay Kostov <admin@nikolay.it>
2015-09-16 16:18:22 +0300

    Santase.AI.DummyPlayer is now Portable Library

--- Source/AI/Santase.AI.DummyPlayer/Properties/AssemblyInfo.cs
+++ Source/AI/Santase.AI.DummyPlayer/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -12,14 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f452d536-573d-4ca0-b24c-17e9e2b425bf")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
