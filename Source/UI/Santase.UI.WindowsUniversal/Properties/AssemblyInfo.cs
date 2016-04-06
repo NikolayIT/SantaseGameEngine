2cea6a43f02f1783950250a94fbb4c9b3ea10967
Nikolay Kostov <admin@nikolay.it>
2016-04-06 17:25:43 +0300

    UI Improvements (grid proportions) in Santase.UI.WindowsUniversal

--- Source/UI/Santase.UI.WindowsUniversal/Properties/AssemblyInfo.cs
+++ Source/UI/Santase.UI.WindowsUniversal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Santase")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Santase (known as Сантасе or 66) is a fast 6-card game for 2 players played with a deck of 24 cards.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Santase")]
-[assembly: AssemblyCopyright("Nikolay.IT © 2015")]
+[assembly: AssemblyCopyright("Nikolay.IT © 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: ComVisible(false)]
