0c1f75fd42e9fba1d23862e179420a5facc8901f
Nikolay Kostov <admin@nikolay.it>
2016-11-26 15:32:52 +0200

    Version 1.5 published to the store

--- Source/AI/Santase.AI.SmartPlayer/Properties/AssemblyInfo.cs
+++ Source/AI/Santase.AI.SmartPlayer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Santase.AI.SmartPlayer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AI for the Santase game engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nikolay.IT")]
 [assembly: AssemblyProduct("Santase.AI.SmartPlayer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -25,6 +25,6 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 [assembly: InternalsVisibleTo("Santase.Tests.GameSimulations")]
