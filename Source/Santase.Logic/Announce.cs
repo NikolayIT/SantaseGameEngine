f47973a56415f07882e1155843e6832413b605ad
Nikolay Kostov <admin@nikolay.it>
2016-12-04 15:38:38 +0200

    Announce and PlayerPosition enumerations documented

--- Source/Santase.Logic/Announce.cs
+++ Source/Santase.Logic/Announce.cs
@@ -2,8 +2,19 @@
 {
     public enum Announce
     {
+        /// <summary>
+        /// No announce
+        /// </summary>
         None = 0,
+
+        /// <summary>
+        /// Announce 20
+        /// </summary>
         Twenty = 20,
+
+        /// <summary>
+        /// Announce 40
+        /// </summary>
         Forty = 40
     }
 }
--- Source/Santase.Logic/PlayerPosition.cs
+++ Source/Santase.Logic/PlayerPosition.cs
@@ -2,8 +2,19 @@
 {
     public enum PlayerPosition
     {
+        /// <summary>
+        /// No one is in position
+        /// </summary>
         NoOne = 0,
+
+        /// <summary>
+        /// The first player is in position
+        /// </summary>
         FirstPlayer = 1,
+
+        /// <summary>
+        /// The second player is in position
+        /// </summary>
         SecondPlayer = 2,
     }
 }
