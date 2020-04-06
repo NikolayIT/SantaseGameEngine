c9680e8b1dc045b42aa583d90a1f5a58bd5c5241
Nikolay Kostov <admin@nikolay.it>
2020-04-06 21:45:11 +0300

    Implemented baseic version of the game for Android using Mobile Blazor Bindings

---
+++ Source/UI/Santase.UI/App.cs
@@ -0,0 +1,35 @@
+﻿namespace Santase.UI
+{
+    using Microsoft.MobileBlazorBindings;
+
+    using Xamarin.Forms;
+
+    public class App : Application
+    {
+        public App()
+        {
+            var host = MobileBlazorBindingsHost.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Register app-specific services
+                    //services.AddSingleton<AppState>();
+                })
+                .Build();
+
+            this.MainPage = new ContentPage();
+            host.AddComponent<GameScreen>(parent: this.MainPage);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
