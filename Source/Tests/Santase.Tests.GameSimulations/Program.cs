30a4fce76a7cfcce44dca22d8e1777473af06e5d
Nikolay Kostov <admin@nikolay.it>
2020-06-27 14:39:02 +0300

    Updated Santase.Tests.GameSimulations to .NET Core 3.1 =========================================================================== 06/27/2020 14:37:29, Mode=Debug, CPUs=24, OS=Microsoft Windows NT 6.2.9200.0, .NET=3.1.5 =========================================================================== Running SmartPlayersGameSimulator... ........................................................................................................................................................................................................00:00:29.5135988 Games: 100,433 - 99,567 (total: 200,000, diff: 866) Round points: 1,770,476 - 1,765,616 (rounds: 2,463,824) Global counters: 199969, 0, 0, 0 (closed: 2,879) =========================================================================== Running SmartAndBestExternalPlayerGameSimulator... ....................................................................................................00:00:20.0183363 Games: 62,523 - 37,477 (total: 100,000, diff: 25046) Round points: 957,544 - 793,019 (rounds: 1,191,734) Global counters: 199969, 0, 0, 0 (closed: 1,300) =========================================================================== Running SmartAndDummyPlayerChangingTrumpSimulator... ....................................................................................................00:00:04.6930239 Games: 99,999 - 01 (total: 100,000, diff: 99998) Round points: 1,157,721 - 82,661 (rounds: 656,580) Global counters: 199969, 0, 0, 0 (closed: 913) =========================================================================== Running SmartAndDummyPlayersSimulator... ....................................................................................................00:00:04.5863823 Games: 100,000 - 00 (total: 100,000, diff: 100000) Round points: 1,157,443 - 73,065 (rounds: 648,472) Global counters: 199969, 0, 0, 0 (closed: 817) ===========================================================================

--- Source/Tests/Santase.Tests.GameSimulations/Program.cs
+++ Source/Tests/Santase.Tests.GameSimulations/Program.cs
@@ -22,6 +22,7 @@
 #elif RELEASE
             Console.Write(", Mode=Release");
 #endif
+            Console.Write($", CPUs={Environment.ProcessorCount}, OS={Environment.OSVersion}, .NET={Environment.Version}");
             Console.WriteLine();
             Console.WriteLine(new string('=', 75));
 
@@ -44,8 +45,8 @@
             var simulationResult = gameSimulator.Simulate(gamesCount);
 
             Console.WriteLine(simulationResult.SimulationDuration);
-            Console.WriteLine($"Games: {simulationResult.FirstPlayerWins:0,0} - {simulationResult.SecondPlayerWins:0,0} (total: {gamesCount:0,0})");
-            Console.WriteLine($"Rounds: {simulationResult.FirstPlayerTotalRoundPoints:0,0} - {simulationResult.SecondPlayerTotalRoundPoints:0,0} (total: {simulationResult.RoundsPlayed:0,0})");
+            Console.WriteLine($"Games: {simulationResult.FirstPlayerWins:0,0} - {simulationResult.SecondPlayerWins:0,0} (total: {gamesCount:0,0}, diff: {simulationResult.FirstPlayerWins - simulationResult.SecondPlayerWins})");
+            Console.WriteLine($"Round points: {simulationResult.FirstPlayerTotalRoundPoints:0,0} - {simulationResult.SecondPlayerTotalRoundPoints:0,0} (rounds: {simulationResult.RoundsPlayed:0,0})");
             Console.WriteLine($"Global counters: {string.Join(", ", GlobalStats.GlobalCounterValues)} (closed: {GlobalStats.GamesClosedByPlayer:0,0})");
             Console.WriteLine(new string('=', 75));
         }
